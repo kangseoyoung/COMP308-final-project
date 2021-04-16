94b6a35532dc87d64661db7dcee44e5a9e3514f4
Bernardo Castro <bernardoc544@gmail.com>
2021-04-16 16:01:27 -0400

    express partially built

--- config/express.js
+++ config/express.js
@@ -2,4 +2,24 @@ var config = require('./config');
 var express = require("express");
 var morgan = require("morgan");
 var compress = require("compress");
-var bodyPress = require("boy-parser");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var session = require("express-session");
+
+module.exports = function() {
+    var app = express();
+
+    if(process.env.NODE_ENV === "development"){
+        app.use(morgan("dev"));
+    }
+    else if(process.env.NODE_ENV === "production"){
+        app.use(compress());
+    }
+    app.use(
+        bodyParser.urlencoded({
+            extended: true,
+        })
+    );
+    app.use(bodyParser.json());
+    return app;
+}
